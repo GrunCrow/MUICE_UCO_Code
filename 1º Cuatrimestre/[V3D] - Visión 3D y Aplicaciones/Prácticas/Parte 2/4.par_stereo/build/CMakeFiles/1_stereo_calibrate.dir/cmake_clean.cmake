7461c5212851d7b622c289b88556d262472f3c25
GrunCrow <57197254+GrunCrow@users.noreply.github.com>
2023-12-18 23:06:41 +0100

    [V3D] - Stereo Sparse

---
+++ 1º Cuatrimestre/[V3D] - Visión 3D y Aplicaciones/Prácticas/Parte 2/4.par_stereo/build/CMakeFiles/1_stereo_calibrate.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "1_stereo_calibrate"
+  "1_stereo_calibrate.pdb"
+  "CMakeFiles/1_stereo_calibrate.dir/1_stereo_calibrate.cpp.o"
+  "CMakeFiles/1_stereo_calibrate.dir/1_stereo_calibrate.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1_stereo_calibrate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 1º Cuatrimestre/[V3D] - Visión 3D y Aplicaciones/Prácticas/Parte 2/4.par_stereo/build/CMakeFiles/2_stereo_checkundistorted.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "2_stereo_checkundistorted"
+  "2_stereo_checkundistorted.pdb"
+  "CMakeFiles/2_stereo_checkundistorted.dir/2_stereo_checkundistorted.cpp.o"
+  "CMakeFiles/2_stereo_checkundistorted.dir/2_stereo_checkundistorted.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_stereo_checkundistorted.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
